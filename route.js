dd5219f0168fb2ae82ffbd9efb72722d9bae23af
alexa0503 <lori.w@live.cn>
2016-03-31 17:38:53 +0800

    bugs

--- route.js
+++ route.js
@@ -40,9 +40,13 @@ module.exports = function(app) {
     app.get('/wx/auth', function(req, res) {
         var app_id = credentials.wx.appId;
         var url = 'http://base_wx.ompchina.net/sns/UserInfo?appId='+app_id+'&openid='+req.query.wechat_openid;
-        request.get(url,function (error,response,body) {
+        request.get(url,function (error,response,body,done) {
             if (!error && response.statusCode == 200) {
                 var data = body;
+                if ( data.errcode){
+                    console.log('授权失败~',data.message);
+                    return done(null,false);
+                }
                 console.log(body);
                 req.session.wxUser = {
                     openid:data.openid,
